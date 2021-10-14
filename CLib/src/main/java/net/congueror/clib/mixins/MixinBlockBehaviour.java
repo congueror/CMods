5f9b64b59d0c338da3ae6b4bfa54ff9dc74961d7
brain.exe <mariovionikos@gmail.com>
2021-10-14 17:03:24 +0300

    0.1.0.1.2

--- CLib/src/main/java/net/congueror/clib/mixins/MixinBlockBehaviour.java
+++ CLib/src/main/java/net/congueror/clib/mixins/MixinBlockBehaviour.java
@@ -1,8 +1,19 @@
 package net.congueror.clib.mixins;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockBehaviour.class)
 public class MixinBlockBehaviour {
+
+    @Inject(at = @At(value = "HEAD"), method = "onPlace")
+    private void onPlace(BlockState pState, Level pLevel, BlockPos pPos, BlockState pOldState, boolean pIsMoving, CallbackInfo ci) {
+        System.out.println("TEST");
+    }
 }
